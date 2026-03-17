5b354ba4b58e0dae5740bdad155df3d0e66454ab
Shakib <mdshakib771575@gmail.com>
2026-03-17 12:31:58 -0700

    complite daisy card

--- src/components/pricingCard/PricingCard.jsx
+++ src/components/pricingCard/PricingCard.jsx
@@ -7,20 +7,20 @@ const PricingCard = ({pricing}) => {
 
 
     return (
-        <div className='border bg-sky-600 rounded-2xl p-4 text-white'>
+        <div className='flex flex-col border bg-sky-600 rounded-2xl p-4 text-white'>
             {/* card header  */}
             <div>
                 <h1 className='text-5xl'>{pricing.name}</h1>
                 <h4 className='text-3xl'>{pricing.price}</h4>
             </div>
             {/* card body */}
-            <div className='bg-amber-300 rounded-2xl p-4'>
+            <div className='bg-amber-300 rounded-2xl p-4 flex-1 '>
                 <p>{pricing.description}</p>
                 {
                  pricing.features.map(feature => <PricingFeature  feature={feature}></PricingFeature>)
                 }
             </div>
-            
+            <button className='btn w-full mt-3'>subcribe</button>
         </div>
     );
 };
--- src/components/pricingOption/PricingOption.jsx
+++ src/components/pricingOption/PricingOption.jsx
@@ -1,5 +1,6 @@
 import React, { use } from 'react';
 import PricingCard from '../pricingCard/PricingCard';
+import DaisyPricingCard from '../DaisyPricingCard/DaisyPricingCard';
 
 const PricingOption = ({pricingPromise}) => {
     const pricingData = use(pricingPromise)
@@ -7,9 +8,12 @@ const PricingOption = ({pricingPromise}) => {
     return (
         <div>
             <h2>Get Our MemberShip :</h2>
-            <div className='grid md:grid-cols-3 gap-8'>
-                {
+            <div className='grid  md:grid-cols-2 lg:grid-cols-3 gap-8'>
+                {/* {
                     pricingData.map(pricing => <PricingCard key={pricing.id} pricing={pricing} ></PricingCard>)
+                } */}
+                {
+                    pricingData.map(pricing =><DaisyPricingCard key={pricing.id}  pricing={pricing} ></DaisyPricingCard>)
                 }
             </div>
         </div>
